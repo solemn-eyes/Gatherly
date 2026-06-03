7857e315a127cf738850c2b07d04f84cf279e783
Jerome Maunga <jerhomemaunga@gmail.com>
2026-06-03 14:05:31 +0300

    Added the notification models

--- backend/notifications/models.py
+++ backend/notifications/models.py
@@ -1,3 +1,29 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
+class Notification(models.Model):
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+
+    type = models.CharField(
+        max_length=50
+    )
+
+    channel = models.CharField(
+        max_length=20
+    )
+
+    status = models.CharField(
+        max_length=20
+    )
+
+    payload = models.JSONField()
+
+    sent_at = models.DateTimeField(
+        null=True,
+        blank=True
+    )
